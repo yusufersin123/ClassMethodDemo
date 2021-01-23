0ac2960eda6fe9d8bcb10e21a626f1888227e695
yusufersin123 <77532521+yusufersin123@users.noreply.github.com>
2021-01-23 09:37:31 -0200

    Update CustomerManager.cs

--- CustomerManager.cs
+++ CustomerManager.cs
@@ -8,9 +8,13 @@ namespace ClassMethodDemo
     { public void Add(Customers customers)
         { if (customers.Dept > 0) { Console.WriteLine("T.c. no:"+customers.Id);
                 Console.WriteLine("Müşteri ismi:"+customers.Name);
-                Console.WriteLine("Kredi Adı"+customers.CreditName);
-                Console.WriteLine("Kredi Zamanı" +customers.Time);
-
-            } }
+                Console.WriteLine("Kredi Adı:"+customers.CreditName);
+                Console.WriteLine("Kredi Zamanı:" +customers.Time);}
+        else {
+                Console.WriteLine("T.c. no:" + customers.Id);
+                Console.WriteLine("Müşteri ismi:" + customers.Name);
+                Console.WriteLine("Kredi Adı:" + customers.CreditName);
+                Console.WriteLine("BORCUNUZ YOKTUR.TEŞEKKÜRLER.");
+            }}
     }
 }
